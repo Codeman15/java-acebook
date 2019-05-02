d56d006f207673a3a091a1adcd8ec4178a577db3
Phil <phil.board90@googlemail.com>
2019-05-02 14:30:19 +0100

    Posts can have new lines in them

--- src/main/js/posts/post.js
+++ src/main/js/posts/post.js
@@ -4,7 +4,9 @@ const Post = (props) => {
 	return (
 		<div className='post-main'>
 			<div className='post-content'>
-				{props.post.content} //adds the content (posts) to the homepage
+			    {props.post.content.split("\n").map((i,key) => { //adds content (posts) to the homepage, and can have new lines in posts
+			        return <div key={key}>{i}</div>;
+                })}
 			</div>
 
             <div className='post-time'>
